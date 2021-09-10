63477bfbde221eeb60dd8b35adb79323350ffb48
Andres Benavides <andres.benavides@eventsite.co>
2021-09-10 18:08:25 -0500

    primera prueba para saber si esta funcionando el metodo del validador

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import os
+import firebase_admin
+import random
+from firebase_admin import credentials
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
-
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+'projectId': 'miso-arquitectura-325522',
+})
 
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
+
+@app.route("/validador")
+def facturador():
+    
+    return { "Pruebas": "error de votacion"  }
 
 
 if __name__ == "__main__":
