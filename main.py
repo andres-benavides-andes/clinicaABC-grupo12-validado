b155f075541b5f058e7b6a07d8cb141ea9e7cf12
Andres Benavides <andres.benavides@eventsite.co>
2021-09-10 21:54:44 -0500

    for para hacer las peticiones

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import firebase_admin
-import random
+import requests
 from firebase_admin import credentials
 
 from flask import Flask, request
@@ -18,9 +18,29 @@ def hello_world():
     return "Hello {}!".format(name)
 
 @app.route("/validador")
-def facturador():
-    
-    return { "Pruebas": "error de votacion"  }
+def validador():
+    for i in range(98):
+        peticiones_ok = 0
+        peticiones_error = 0
+        id_paiente = i + 1
+        servicio_factura_1 = requests.get(f'https://facturador-1-ndwos22ynq-uc.a.run.app/facturador?id_paciente={id_paiente}')
+        servicio_factura_2 = requests.get(f'https://facturador-2-ndwos22ynq-uc.a.run.app/facturador?id_paciente={id_paiente}')
+        servicio_factura_3 = requests.get(f'https://facturador-3-ndwos22ynq-uc.a.run.app/facturador?id_paciente={id_paiente}')
+
+        
+        if servicio_factura_1.status_code == 200 and servicio_factura_2.status_code == 200 and servicio_factura_3.status_code == 200:
+            rta_1 = servicio_factura_1.json()
+            rta_2 = servicio_factura_1.json()
+            rta_3 = servicio_factura_3.json()
+            if rta_1["total"] == rta_2["total"]:
+                if rta_1["total"] == rta_3["total"]:
+                    peticiones_ok += 1
+                else:
+                    peticiones_error +=1
+            else:
+                peticiones_error +=1
+
+    return {"peticiones correctas":peticiones_ok,"peticiones con":peticiones_error}
 
 
 if __name__ == "__main__":
